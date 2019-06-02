7335fdc9c11c53d8184574183c111a82fa418668
Jubaid Hoss <IMAC@IMACs-iMac.home>
2019-06-02 16:24:59 -0400

    add post-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PostCreateComponenet} from './posts/post-create/post-create.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponenet
   ],
   imports: [
     BrowserModule
